9e843b057d02c2baa4211f40bdd2636b35acf210
Pham_Hieu <hieuptch2012001@fpt.edu.vn>
2021-05-12 12:13:02 +0700

    Update

---
+++ S_CV_hinh_tron/Circle.py
@@ -0,0 +1,25 @@
+import math
+
+
+class circle():
+    def __init__(self, radius):
+        self.radius = radius
+
+    def dien_tich(self):
+        S = self.radius ** 2
+        return S
+
+    def chu_vi(self):
+        cv = 2 * math.pi * self.radius
+        return cv
+
+
+def main():
+    radius = int(input("Nhập bán kính: "))
+    bk = circle(radius)
+    print('Diện tích hình tròn: ', bk.dien_tich())
+    print('Chu vi hình tròn: ', bk.chu_vi())
+
+
+if __name__ == '__main__':
+    main()
