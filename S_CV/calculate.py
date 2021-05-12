aa58eae22d0e3b34c9e2c324f86876409549b770
Pham_Hieu <hieuptch2012001@fpt.edu.vn>
2021-05-12 13:53:44 +0700

    update 20:53 12/5/2021
    update file calculate.py sang biến private

--- S_CV/calculate.py
+++ S_CV/calculate.py
@@ -1,35 +1,40 @@
 class numerals():
-    number1 = 0
-    number2 = 0
+    __number1 = 0
+    __number2 = 0
 
     def __init__(self, number1, number2):
-        self.number1 = number1
-        self.number2 = number2
+        self.__number1 = number1
+        self.__number2 = number2
 
     def sum(self):
-        print('a + b = ', self.number1 + self.number2)
+        return self.__number1 + self.__number2
 
     def sub(self):
-        print('a - b = ', self.number1 - self.number2)
+        return self.__number1 - self.__number2
 
     def mul(self):
-        print('a * b = ', self.number1 * self.number2)
+        return self.__number1 * self.__number2
 
     def div(self):
-        if self.number2 != 0:
-            print('a / b = ', self.number1 / self.number2)
+        if self.__number2 != 0:
+            return self.__number1 / self.__number2
         else:
             print("erorr")
 
 
 def main():
 
-    nub = numerals(5, 3)
+    nub = numerals(6, 2)
 
-    print(nub.sum())
-    print(nub.sub())
-    print(nub.mul())
-    print(nub.div())
+    print('a = ', nub._numerals__number1)
+    print('b = ', nub._numerals__number2)
+
+    print("----------------")
+
+    print('a + b = ', nub.sum())
+    print('a - b = ', nub.sub())
+    print('a * b = ', nub.mul())
+    print('a / b = ', nub.div())
 
 
 if __name__ == '__main__':
